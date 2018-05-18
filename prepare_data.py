3119fea3517f69027600c9d3fc2aa9d47e35f6c0
Nick-J7 <jb.jeong07@gmail.com>
2018-05-18 15:22:44 +0900

    Moidfy prepare_data.py and add test code

--- prepare_data.py
+++ prepare_data.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 import os
 import os.path as osp
 import time
@@ -9,17 +10,15 @@ import pandas as pd
 import pdb
 
 
-RAW_DATA_DIR = 'data_raw'
-COOKED_DATA_DIR = 'cooked_data'
-
-
-def print_data_info(folders):
-    """Print information of data.
-        Inputs:
+def get_data_count(folders):
+    """Get information about number of data.
+        Args:
            folders (list): full path of raw data directory such as 
                 ['data_raw/normal_1', 'data_raw/normal_2', ..]
-    """
 
+        Returns:
+            int: number of all data
+    """
     rec_filename = 'airsim_rec.txt'
 
     num_of_all_data = 0
@@ -27,75 +26,51 @@ def print_data_info(folders):
         current_dataframe = pd.read_csv(osp.join(folder, rec_filename), sep='\t')
         num_of_all_data += current_dataframe.shape[0]
 
-    print("Number of all data: {}".format(num_of_all_data))
-    print("Maybe the number of output data is {}.\n" 
-          "Because when each txt file is processed, the first and the last row is excluded."
-          .format(num_of_all_data - 2 * len(folders)))
+    return num_of_all_data
 
 
 def split_data(data, split_ratio):
-    """Split data into three groups by using split ratio
-        Inputs:
+    """Split data into three groups by using split ratio.
+        Args:
             data (list): list of tuple data
             split_ratio (tuple): ratio of split 
                 0 -> train
                 1 -> validation
                 2 -> test
         Returns:
-            train (list): splitted list of tuple data
-            val (list)
-            test (list)
+            tuple: splitted data 
+                0 -> train(list)
+                1 -> validation(list)
+                2 -> test(list)
     """
-
     num_train = int(len(data) * split_ratio[0])
     num_val = int(len(data) * split_ratio[1])
 
     train = data[:num_train]
     val = data[num_train:num_train + num_val]
     test = data[num_train + num_val:]
-    print("Number of train:", num_train)
-    print("Number of validation:", num_val)
-    print("Number of test:", len(data) - num_train - num_val)
     
     return train, val, test
 
 
 def generate_data(folders):
-    """Print information of data.
-        Inputs:
-           folders (list): full path of raw data directory such as 
+    """Generate cooked data.
+        Args:
+            folders (list): full path of raw data directory such as 
                 ['data_raw/normal_1', 'data_raw/normal_2', ..]
+        Returns:
+            list: all data which is shuffled (list of tuple)
     """
-
-    fieldnames = ['Imagepath', 'Timestamp', 'Speed (kmph)', 'Throttle', 'Brake', 'Gear', 'Label']
-
-    def write_data(data, filename):
-        csvfile = open(osp.join(COOKED_DATA_DIR, filename), 'w')
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for i in range(len(data)):
-            writer.writerow({
-                'Imagepath': data[i][0],
-                'Timestamp': data[i][1],
-                'Speed (kmph)': data[i][2],
-                'Throttle': data[i][3],
-                'Brake': data[i][4],
-                'Gear': data[i][5],
-                'Label': data[i][6]
-                })
-        csvfile.close()
-
-
     rec_filename = 'airsim_rec.txt'
     all_data = []
 
-    print('Generating data...')
     for folder in folders:
         current_dataframe = pd.read_csv(osp.join(folder, rec_filename), sep='\t')
 
         for i in range(1, current_dataframe.shape[0] - 1):
             # Set path of image for loading in torch.utils.data.DataLoader
-            imagepath = osp.join(folder, 'images', current_dataframe.iloc[i]['ImageName'])
+            imagename = current_dataframe.iloc[i]['ImageName']
+            imagepath = osp.join(folder, 'images', imagename)
 
             # Label is average of {(t-1), t, (t+1) steering angle}
             current_label = (current_dataframe.iloc[i-1]['Steering'] +
@@ -117,35 +92,71 @@ def generate_data(folders):
             all_data.append(current_tuple)
 
     random.shuffle(all_data)
+    return all_data
     
+
+def make_data(folders, output_dir):
+    """Print information of data.
+        Args:
+            folders (list): full path of raw data directory such as 
+                ['data_raw/normal_1', 'data_raw/normal_2', ..]
+            output_dir (string): directory for saving output file 
+    """
+
+    # Print number of data
+    num_of_all_data = get_data_count(folders)
+    logging.info("Number of all data: {}."
+          .format(num_of_all_data))
+
+    # Generate data
+    logging.info('Generating data...')
+    s_time = time.time()
+    all_data = generate_data(folders)
+    e_time = time.time()
+    logging.info('Elapsed time for generating data: {}'.format(e_time - s_time))
+
+    # Split data
     train_eval_test_split = (0.7, 0.2, 0.1)
-    train, validation, test= split_data(all_data, train_eval_test_split)
+    train, validation, test = split_data(all_data, train_eval_test_split)
+    logging.info("Number of train: {}".format(len(train)))
+    logging.info("Number of validation: {}".format(len(validation)))
+    logging.info("Number of test: {}".format(len(test)))
+
+    # Write data
+    fieldnames = ['Imagepath', 'Timestamp', 'Speed (kmph)', 'Throttle', 'Brake', 'Gear', 'Label']
+
+    def write_data(data, filename):
+        csvfile = open(osp.join(output_dir, filename), 'w')
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        for i in range(len(data)):
+            writer.writerow({
+                'Imagepath': data[i][0],
+                'Timestamp': data[i][1],
+                'Speed (kmph)': data[i][2],
+                'Throttle': data[i][3],
+                'Brake': data[i][4],
+                'Gear': data[i][5],
+                'Label': data[i][6]
+                })
+        csvfile.close()
+
 
     write_data(all_data, 'all_data.csv')
     write_data(train, 'train.csv')
     write_data(validation, 'validation.csv')
     write_data(test, 'test.csv')
 
-    print('Finish !!!') 
-
-
-def make_data(folders):
-    """Print information of data.
-        Inputs:
-           folders (list): full path of raw data directory such as 
-                ['data_raw/normal_1', 'data_raw/normal_2', ..]
-    """
-
-    print_data_info(folders)
-
-    s_time = time.time()
-    generate_data(folders)
-    e_time = time.time()
-    print('Elapsed time: {}'.format(e_time - s_time))
+    logging.info('Finish !!!') 
 
 
 if __name__ == '__main__':
 
+    logging.basicConfig(level=logging.INFO)
+
+    RAW_DATA_DIR = 'data_raw'
+    COOKED_DATA_DIR = 'cooked_data'
+
     data_folders = sorted(os.listdir(RAW_DATA_DIR))
     full_path_data_folders = [osp.join(RAW_DATA_DIR, f) for f in data_folders]
-    make_data(full_path_data_folders)
+    make_data(full_path_data_folders, COOKED_DATA_DIR)
---
+++ test/test_prepare_data.py
@@ -0,0 +1,60 @@
+import os
+
+import unittest
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from prepare_data import *
+
+
+class Tester(unittest.TestCase):
+    """Test prepare_data."""
+
+    RAW_DATA_DIR = 'test/assets/data_raw'
+    COOKED_DATA_DIR = 'test/assets/data/cooked_data'
+    
+    data_folders = sorted(os.listdir('test/assets/data_raw'))
+    full_path_data_folders = [os.path.join('test/assets/data_raw', f) for f in data_folders]
+
+    def test_get_data_count(self):
+
+        rec_filename = 'airsim_rec.txt'
+
+        num_data = 0
+        for folder in Tester.full_path_data_folders:
+            with open(os.path.join(folder, rec_filename)) as f:
+                num_lines = sum(1 for line in f)
+            # The first row is header.
+            num_lines -= 1
+            num_data += num_lines
+
+        num_of_all_data = get_data_count(Tester.full_path_data_folders)
+
+        self.assertEqual(num_data, num_of_all_data)
+
+    def test_split_data(self):
+
+        num_data = 10
+        data = [0 for _ in range(num_data)]
+        split_ratio = (0.7, 0.2, 0.1)
+        train, validation, test = split_data(data, split_ratio)
+
+        self.assertEqual(len(train), int(num_data * split_ratio[0]))
+        self.assertEqual(len(validation), int(num_data * split_ratio[1]))
+        self.assertEqual(len(test), int(num_data * split_ratio[2]))
+        self.assertEqual(len(train) + len(validation) + len(test), num_data) 
+
+    def test_generate_data(self):
+
+        num_of_all_data = get_data_count(Tester.full_path_data_folders)
+
+        # When each txt file is processed, the first and the last row is excluded.
+        num_of_cooked_data = num_of_all_data - 2 * len(Tester.full_path_data_folders)
+
+        all_data = generate_data(Tester.full_path_data_folders)
+
+        self.assertEqual(len(all_data), num_of_cooked_data)
+
+
+if __name__ == '__main__':
+    unittest.main()
